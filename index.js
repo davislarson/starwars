a69ce47b9259d8d3c4028c1267d7c078883e5c40
davissl <davissl@byu.edu>
2024-05-31 12:58:38 -0600

    create recursive function

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const attributes = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
-let id = Math.random().toFixed(2)*100
-console.log(id)
-let chosenAttribute = attributes[Math.floor(Math.random()*attributes.length)]
-console.log(chosenAttribute)
-let extension = `${chosenAttribute}/${id}`;
+const callStarwars = function () {
+    const attributes = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
+    let id = Math.random().toFixed(2)*100 - 50
+    console.log(id)
+    let chosenAttribute = attributes[Math.floor(Math.random()*attributes.length)]
+    console.log(chosenAttribute)
+    let extension = `${chosenAttribute}/${id}`;
+    apiURL = "https://swapi.dev/api/" + `${extension}`;
 
-apiURL = "https://swapi.dev/api/" + `${extension}`;
-
-let result = fetch(apiURL)
+    let result = fetch(apiURL)
     .then(res => res.json())
     .then(data => {
         if (data.name != undefined){
-        document.getElementById("top").innerHTML = data.name, data.films
+        document.getElementById("top").innerHTML = data.name
     } else {
-        
+        callStarwars()
     }
     })
+}
